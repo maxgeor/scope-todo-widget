48371199ce1bc1ac3613cd6edfed126d5697dac2
Max Georgopoulos <maxgeorgopoulos@Maxs-MacBook-Air.local>
2021-11-01 21:52:39 -0400

    added visual tweaks and sample code

--- code.js
+++ code.js
@@ -1,49 +1,104 @@
 const { widget } = figma;
-const { useSyncedState, AutoLayout, Text, SVG, Rectangle } = widget;
+const { useSyncedState, useEffect, AutoLayout, Text, SVG, Rectangle } = widget;
+// import { nanoid as createId } from 'nanoid'
+// const { v4: createId } = require('uuid');
 function ScopedTodoCard() {
-    const [todos, setTodos] = useSyncedState('todos', []);
-    const Todo = ({ title, done, outOfScope }) => {
-        return (figma.widget.h(AutoLayout, { direction: 'horizontal', verticalAlignItems: 'center', spacing: 'auto', width: 350 },
-            figma.widget.h(AutoLayout, { spacing: 8, verticalAlignItems: 'center', direction: 'horizontal' },
-                figma.widget.h(AutoLayout, { hidden: done || outOfScope ? true : false, width: 20, height: 20, verticalAlignItems: 'center', horizontalAlignItems: 'center' },
-                    figma.widget.h(Rectangle, { width: 16, height: 16, fill: '#FFF', stroke: '#BABABA', strokeWidth: 1, cornerRadius: 4 })),
-                figma.widget.h(SVG, { hidden: done === false || outOfScope ? true : false, src: `
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#00A354">
-                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-              </svg>  
+    const [todos, setTodos] = useSyncedState('todos', [
+        // {
+        //   title: 'odjfvkjdbfvkjdbkdf',
+        //   done: true,
+        //   outOfScope: true,
+        // },
+        {
+            title: 'When you click the checkbox, it changes that todos “Done” state to true',
+            done: false,
+            outOfScope: false,
+        },
+        {
+            title: 'Find a way to fix the sink without messing it up',
+            done: true,
+            outOfScope: false,
+        },
+    ]);
+    useEffect(() => {
+        figma.ui.onmessage = (msg) => {
+            if (msg.type === 'showToast') {
+                figma.notify('Hello widget');
+            }
+            if (msg.type === 'close') {
+                figma.closePlugin();
+            }
+        };
+    });
+    const Todo = (props) => {
+        return (figma.widget.h(AutoLayout, { direction: 'horizontal', verticalAlignItems: 'center', spacing: 'auto', width: 325, overflow: 'visible' },
+            figma.widget.h(AutoLayout, { spacing: 8, verticalAlignItems: 'center', direction: 'horizontal', overflow: 'visible' },
+                figma.widget.h(AutoLayout, { hidden: props.done || props.outOfScope ? true : false, width: 20, height: 20, verticalAlignItems: 'center', horizontalAlignItems: 'center' },
+                    figma.widget.h(Rectangle, { width: 16, height: 16, fill: '#FFF', stroke: '#B2B2B2', strokeWidth: 1, cornerRadius: 4 })),
+                figma.widget.h(SVG, { hidden: props.done === false || props.outOfScope ? true : false, src: `
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M6 2C3.79086 2 2 3.79086 2 6V14C2 16.2091 3.79086 18 6 18H14C16.2091 18 18 16.2091 18 14V6C18 3.79086 16.2091 2 14 2H6ZM14.2474 8.66436C14.6143 8.25158 14.5771 7.61951 14.1644 7.25259C13.7516 6.88567 13.1195 6.92285 12.7526 7.33564L9.45718 11.043L7.70711 9.29289C7.31658 8.90237 6.68342 8.90237 6.29289 9.29289C5.90237 9.68342 5.90237 10.3166 6.29289 10.7071L8.79289 13.2071C8.98764 13.4019 9.25408 13.5077 9.52937 13.4996C9.80467 13.4915 10.0644 13.3702 10.2474 13.1644L14.2474 8.66436Z" fill="#4AB393"/>
+              </svg>
             ` }),
-                figma.widget.h(Rectangle, { width: 20, height: 20, fill: '#ebebeb', hidden: !outOfScope }),
-                figma.widget.h(Text, { fill: done || outOfScope ? "#727272" : "#101010", fontSize: done || outOfScope ? 13 : 16, textDecoration: done && !outOfScope ? 'strikethrough' : 'none' }, title)),
+                figma.widget.h(Rectangle, { width: 20, height: 20, fill: '#f0f0f0', hidden: !props.outOfScope }),
+                figma.widget.h(Text, { fill: props.outOfScope ? "#6D6D6D" : props.done ? "#949494" : "#000", fontSize: props.done || props.outOfScope ? 13 : 16, textDecoration: props.done && !props.outOfScope ? 'strikethrough' : 'none' }, props.title)),
             figma.widget.h(SVG, { src: `
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#AEAEAE">
               <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
             </svg>
-          ` })));
+          `, onClick: () => new Promise((resolve) => {
+                    figma.showUI(__html__);
+                }) })));
     };
     const notDoneTodos = todos.filter(todo => !todo.done && !todo.outOfScope)
-        .map(todo => figma.widget.h(Todo, { title: todo.title, done: todo.done, outOfScope: todo.outOfScope }));
+        .map(todo => {
+        return figma.widget.h(Todo
+        // key={todo.id}
+        // id={todo.id}
+        , { 
+            // key={todo.id}
+            // id={todo.id}
+            title: todo.title, done: todo.done, outOfScope: todo.outOfScope });
+    });
     const doneTodos = todos.filter(todo => todo.done && !todo.outOfScope)
-        .map(todo => figma.widget.h(Todo, { title: todo.title, done: todo.done, outOfScope: todo.outOfScope }));
+        .map(todo => {
+        return figma.widget.h(Todo
+        // key={todo.id}
+        // id={todo.id}
+        , { 
+            // key={todo.id}
+            // id={todo.id}
+            title: todo.title, done: todo.done, outOfScope: todo.outOfScope });
+    });
     const outOfScopeTodos = todos.filter(todo => todo.outOfScope)
-        .map(todo => figma.widget.h(Todo, { title: todo.title, done: todo.done, outOfScope: todo.outOfScope }));
-    return (figma.widget.h(AutoLayout, { direction: 'vertical', cornerRadius: 8, fill: '#fafafa', stroke: '#E5E5E5', strokeWidth: 1 },
+        .map(todo => {
+        return figma.widget.h(Todo
+        // key={todo.id}
+        // id={todo.id}
+        , { 
+            // key={todo.id}
+            // id={todo.id}
+            title: todo.title, done: todo.done, outOfScope: todo.outOfScope });
+    });
+    return (figma.widget.h(AutoLayout, { direction: 'vertical', cornerRadius: 8, fill: '#fff', stroke: '#E5E5E5', strokeWidth: 1 },
         figma.widget.h(AutoLayout, { direction: 'vertical', spacing: 24, padding: 24 },
             figma.widget.h(AutoLayout, { direction: 'vertical', spacing: 8 },
                 notDoneTodos.length > 0 && notDoneTodos,
                 figma.widget.h(AutoLayout, { direction: 'horizontal', verticalAlignItems: 'center', spacing: 8, onClick: () => setTodos([
                         ...todos,
                         {
+                            // id: createId(),
                             title: "New todo",
                             done: false,
                             outOfScope: false
                         }
                     ]) },
                     figma.widget.h(SVG, { src: `
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#8f8f8f">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#949494">
                 <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
               </svg>` }),
-                    figma.widget.h(Text, { fill: '#727272', fontWeight: 600, fontSize: 14 }, "Add a todo"))),
+                    figma.widget.h(Text, { fill: '#949494', fontSize: 14, fontWeight: 700 }, "Add a todo"))),
             figma.widget.h(AutoLayout, { direction: 'vertical', spacing: 8, hidden: !doneTodos.length }, doneTodos)),
-        figma.widget.h(AutoLayout, { direction: 'vertical', horizontalAlignItems: 'center', spacing: 8, padding: 24, fill: '#ebebeb' }, outOfScopeTodos.length === 0 ? figma.widget.h(Rectangle, { width: 350, height: 4, fill: '#ebebeb' }) : outOfScopeTodos)));
+        figma.widget.h(AutoLayout, { direction: 'vertical', horizontalAlignItems: 'center', spacing: 8, padding: 24, fill: '#F0F0F0' }, outOfScopeTodos.length === 0 ? figma.widget.h(Rectangle, { width: 325, height: 4, fill: '#F0F0F0' }) : outOfScopeTodos)));
 }
 widget.register(ScopedTodoCard);
--- code.tsx
+++ code.tsx
@@ -1,24 +1,56 @@
 const { widget } = figma
-const { useSyncedState, AutoLayout, Text, SVG, Rectangle } = widget
+const { useSyncedState, useEffect, AutoLayout, Text, SVG, Rectangle } = widget
+
+// import { nanoid as createId } from 'nanoid'
+// const { v4: createId } = require('uuid');
 
 function ScopedTodoCard() {
-  const [todos, setTodos] = useSyncedState('todos', [])
+  const [todos, setTodos] = useSyncedState('todos', [
+    // {
+    //   title: 'odjfvkjdbfvkjdbkdf',
+    //   done: true,
+    //   outOfScope: true,
+    // },
+    {
+      title: 'When you click the checkbox, it changes that todos “Done” state to true',
+      done: false,
+      outOfScope: false,
+    },
+    {
+      title: 'Find a way to fix the sink without messing it up',
+      done: true,
+      outOfScope: false,
+    },
+  ])
 
-  const Todo = ({title, done, outOfScope}) => {
+  useEffect(() => {
+    figma.ui.onmessage = (msg) => {
+      if (msg.type === 'showToast') {
+        figma.notify('Hello widget')
+      }
+      if (msg.type === 'close') {
+        figma.closePlugin()
+      }
+    }
+  })
+
+  const Todo = (props) => {
     return (
       <AutoLayout
         direction={'horizontal'}
         verticalAlignItems={'center'}
         spacing={'auto'}
-        width={350}
-        >
+        width={325}
+        overflow={'visible'}
+      >
         <AutoLayout
           spacing={8}
           verticalAlignItems={'center'}
           direction={'horizontal'}
+          overflow={'visible'}
         >
           <AutoLayout 
-            hidden={done || outOfScope ? true : false}
+            hidden={props.done || props.outOfScope ? true : false}
             width={20} 
             height={20} 
             verticalAlignItems={'center'} 
@@ -28,32 +60,32 @@ function ScopedTodoCard() {
               width={16}
               height={16}
               fill={'#FFF'}
-              stroke={'#BABABA'}
+              stroke={'#B2B2B2'}
               strokeWidth={1}
               cornerRadius={4}
             />
           </AutoLayout>
           <SVG 
-            hidden={done === false || outOfScope ? true : false}
+            hidden={props.done === false || props.outOfScope ? true : false}
             src={`
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#00A354">
-                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-              </svg>  
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M6 2C3.79086 2 2 3.79086 2 6V14C2 16.2091 3.79086 18 6 18H14C16.2091 18 18 16.2091 18 14V6C18 3.79086 16.2091 2 14 2H6ZM14.2474 8.66436C14.6143 8.25158 14.5771 7.61951 14.1644 7.25259C13.7516 6.88567 13.1195 6.92285 12.7526 7.33564L9.45718 11.043L7.70711 9.29289C7.31658 8.90237 6.68342 8.90237 6.29289 9.29289C5.90237 9.68342 5.90237 10.3166 6.29289 10.7071L8.79289 13.2071C8.98764 13.4019 9.25408 13.5077 9.52937 13.4996C9.80467 13.4915 10.0644 13.3702 10.2474 13.1644L14.2474 8.66436Z" fill="#4AB393"/>
+              </svg>
             `}
           />
           <Rectangle 
               width={20}
               height={20}
-              fill={'#ebebeb'}
-              hidden={!outOfScope}
+              fill={'#f0f0f0'}
+              hidden={!props.outOfScope}
             />
           <Text 
-            fill={done || outOfScope ? "#727272" : "#101010"}
-            fontSize={done || outOfScope ? 13 : 16}
-            textDecoration={done && !outOfScope ? 'strikethrough' : 'none'}
+            fill={ props.outOfScope ? "#6D6D6D" : props.done ? "#949494" : "#000"}
+            fontSize={props.done || props.outOfScope ? 13 : 16}
+            textDecoration={props.done && !props.outOfScope ? 'strikethrough' : 'none'}
             // onClick={() => makeTextEditable(this)}
           >
-            {title}
+            {props.title}
           </Text>
         </AutoLayout>
         <SVG
@@ -62,6 +94,10 @@ function ScopedTodoCard() {
               <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
             </svg>
           `}
+          onClick={() =>
+            new Promise((resolve) => {
+              figma.showUI(__html__)
+            })}
         />
         {/* <AutoLayout
           direction={"vertical"}
@@ -103,39 +139,45 @@ function ScopedTodoCard() {
 
   const notDoneTodos = 
     todos.filter(todo => !todo.done && !todo.outOfScope)
-         .map(todo =>
-           <Todo 
-             title={todo.title}
-             done={todo.done}
-             outOfScope={todo.outOfScope}
-           />
-         )
+      .map(todo => {
+        return <Todo 
+          // key={todo.id}
+          // id={todo.id}
+          title={todo.title}
+          done={todo.done}
+          outOfScope={todo.outOfScope}
+        />
+      })
   
   const doneTodos = 
     todos.filter(todo => todo.done && !todo.outOfScope)
-         .map(todo =>
-           <Todo 
-             title={todo.title}
-             done={todo.done}
-             outOfScope={todo.outOfScope}
-           />
-         )
+      .map(todo => {
+        return <Todo 
+          // key={todo.id}
+          // id={todo.id}
+          title={todo.title}
+          done={todo.done}
+          outOfScope={todo.outOfScope}
+        />
+      })
   
   const outOfScopeTodos = 
     todos.filter(todo => todo.outOfScope)
-         .map(todo =>
-           <Todo 
-             title={todo.title}
-             done={todo.done}
-             outOfScope={todo.outOfScope}
-           />
-         )
+      .map(todo => {
+        return <Todo 
+          // key={todo.id}
+          // id={todo.id}
+          title={todo.title}
+          done={todo.done}
+          outOfScope={todo.outOfScope}
+        />
+      })
 
   return (
     <AutoLayout
       direction={'vertical'}
       cornerRadius={8}
-      fill={'#fafafa'}
+      fill={'#fff'}
       stroke={'#E5E5E5'}
       strokeWidth={1}
     >
@@ -157,18 +199,24 @@ function ScopedTodoCard() {
             onClick={() => setTodos([
               ...todos,
               {
+                // id: createId(),
                 title: "New todo",
                 done: false,
                 outOfScope: false
               }
             ])}
           >
+            {/* <SVG src={`
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M6 2C3.79086 2 2 3.79086 2 6V14C2 16.2091 3.79086 18 6 18H14C16.2091 18 18 16.2091 18 14V6C18 3.79086 16.2091 2 14 2H6ZM11 7C11 6.44772 10.5523 6 10 6C9.44772 6 9 6.44772 9 7V9H7C6.44772 9 6 9.44772 6 10C6 10.5523 6.44772 11 7 11H9V13C9 13.5523 9.44772 14 10 14C10.5523 14 11 13.5523 11 13V11H13C13.5523 11 14 10.5523 14 10C14 9.44772 13.5523 9 13 9H11V7Z" fill="#8F8F8F"/>
+              </svg>`}
+            /> */}
             <SVG src={`
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#8f8f8f">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#949494">
                 <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
               </svg>`}
             />
-            <Text fill={'#727272'} fontWeight={600} fontSize={14}>Add a todo</Text>
+            <Text fill={'#949494'} fontSize={14} fontWeight={700}>Add a todo</Text>
           </AutoLayout>
         </AutoLayout>
         <AutoLayout
@@ -184,9 +232,9 @@ function ScopedTodoCard() {
         horizontalAlignItems={'center'}
         spacing={8}
         padding={24}
-        fill={'#ebebeb'}
+        fill={'#F0F0F0'}
       >
-        {outOfScopeTodos.length === 0 ? <Rectangle width={350} height={4} fill={'#ebebeb'}></Rectangle> : outOfScopeTodos}
+        {outOfScopeTodos.length === 0 ? <Rectangle width={325} height={4} fill={'#F0F0F0'}></Rectangle> : outOfScopeTodos}
       </AutoLayout>
     </AutoLayout>
   )
