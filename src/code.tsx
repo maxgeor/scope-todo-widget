bc9823058a527e12b397a82948a62510ce183b7b
Max Georgopoulos <maxgeorgopoulos@Maxs-MacBook-Air.local>
2021-11-06 23:08:26 -0400

    abstracted handleMove to handleChange

--- src/code.tsx
+++ src/code.tsx
@@ -27,10 +27,13 @@ import { nanoid as createId } from 'nanoid/non-secure'
 function ScopedTodoCard() {
   const [todos, setTodos] = useSyncedState('todos', [])
   
-  function handleMove (id: string, outOfScope: boolean) {
+  function handleChange (id: string, changedPropName: string, changedPropValue: any) {
     const targetTodo = todos.find(todo => todo.id === id)
-    targetTodo.outOfScope = !outOfScope
-    
+    if (changedPropName === "done") {
+      targetTodo.done = !changedPropValue
+    } else if (changedPropName === "outOfScope") {
+      targetTodo.outOfScope = !changedPropValue
+    }
     setTodos([...todos.filter(todo => todo.id !== id), targetTodo])
   }
 
@@ -49,7 +52,7 @@ function ScopedTodoCard() {
         >
           <SVG
             hidden={done || outOfScope}
-            // onClick={() => handleCheck(key, id, title, outOfScope)}
+            onClick={() => handleChange(id, "done", done)}
             src={`
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="4.5" y="4.5" width="15" height="15" rx="3.5" stroke="#b2b2b2"/>
@@ -58,7 +61,7 @@ function ScopedTodoCard() {
           />
           <SVG 
             hidden={!done || outOfScope}
-            // onClick={() => handleCheck(key, id, title, outOfScope)}
+            onClick={() => handleChange(id, "done", done)}
             src={`
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4C5.79086 4 4 5.79086 4 8V16C4 18.2091 5.79086 20 8 20H16C18.2091 20 20 18.2091 20 16V8C20 5.79086 18.2091 4 16 4H8ZM16.2474 10.6644C16.6143 10.2516 16.5771 9.61951 16.1644 9.25259C15.7516 8.88567 15.1195 8.92285 14.7526 9.33564L11.4572 13.043L9.70711 11.2929C9.31658 10.9024 8.68342 10.9024 8.29289 11.2929C7.90237 11.6834 7.90237 12.3166 8.29289 12.7071L10.7929 15.2071C10.9876 15.4019 11.2541 15.5077 11.5294 15.4996C11.8047 15.4915 12.0644 15.3702 12.2474 15.1644L16.2474 10.6644Z" fill="#4AB393"/>
@@ -82,7 +85,7 @@ function ScopedTodoCard() {
           </TextBlock>
         </AutoLayout>
         <SVG
-          onClick={() => handleMove(id, outOfScope)}
+          onClick={() => handleChange(id, "outOfScope", outOfScope)}
           src={`
             <svg width="24" height="24" viewBox="0 0 24 24" fill="${outOfScope ? "#919191" : "#949494"}" xmlns="http://www.w3.org/2000/svg">
               <rect x="4" y="10" width="4" height="4" rx="2" />
