281fa7149fcc5e66fda337d23f8c5bb672ff0d10
Max Georgopoulos <73665502+maxgeor@users.noreply.github.com>
2022-03-26 10:48:48 -0400

    Fixed bug with add buttton

--- src/code.tsx
+++ src/code.tsx
@@ -6,6 +6,14 @@ function TodoWidget() {
   const widgetId = useWidgetId()
   const [todos, setTodos] = useSyncedState<any[]>('todos', [])
 
+  interface TodoProps {
+    key: string,
+    id: string,
+    title: string, 
+    done: boolean, 
+    outOfScope: boolean
+  } 
+
   useEffect(() => {
     figma.ui.onmessage = msg => {
       if (msg.type === 'update-title') {
@@ -36,15 +44,16 @@ function TodoWidget() {
 
   function handleChange (id: string, changedProp: string, changedPropValue: any) {
     const updateTodo = (todo: {title: string, done: boolean, outOfScope: boolean}) => {
-      if (changedProp === 'title') {
-        todo.title = changedPropValue
-      } else if (changedProp === 'done') {
-        todo.done = !changedPropValue
-      } else if (changedProp === 'outOfScope') {
-        todo.done = false
-        todo.outOfScope = changedPropValue
+      switch(changedProp) {
+        case 'title':
+          return {...todo, title: changedPropValue}
+        case 'done':
+          return {...todo, done: !changedPropValue}
+        case 'outOfScope':
+          return {...todo, outOfScope: changedPropValue}
+        default:
+          return todo
       }
-      return todo
     }
     const freshTodos = todos.map(todo => {
       if(todo.id === id) {
@@ -69,7 +78,7 @@ function TodoWidget() {
     )
   }
 
-  const Todo = ({ key, id, title, done, outOfScope }) => {
+  const Todo = (props: TodoProps) => {
     return (
       <AutoLayout
         direction={'horizontal'}
@@ -83,8 +92,8 @@ function TodoWidget() {
           spacing={8}
         >
           <SVG
-            hidden={done || outOfScope}
-            onClick={() => handleChange(id, "done", done)}
+            hidden={props.done || props.outOfScope}
+            onClick={() => handleChange( props.id, "done",  props.done)}
             src={`
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="2.5" y="2.5" width="15" height="15" rx="3.5" fill="white" stroke="#aeaeae"/>
@@ -92,8 +101,8 @@ function TodoWidget() {
             `}
           />
           <SVG 
-            hidden={!done || outOfScope}
-            onClick={() => handleChange(id, "done", done)}
+            hidden={!props.done || props.outOfScope}
+            onClick={() => handleChange( props.id, "done",  props.done)}
             src={`
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M6 2C3.79086 2 2 3.79086 2 6V14C2 16.2091 3.79086 18 6 18H14C16.2091 18 18 16.2091 18 14V6C18 3.79086 16.2091 2 14 2H6ZM14.3408 8.74741C14.7536 8.28303 14.7118 7.57195 14.2474 7.15916C13.783 6.74638 13.0719 6.78821 12.6592 7.25259L10.6592 9.50259L9.45183 10.8608L7.7955 9.2045C7.35616 8.76516 6.64384 8.76516 6.2045 9.2045C5.76517 9.64384 5.76517 10.3562 6.2045 10.7955L8.7045 13.2955C8.92359 13.5146 9.22334 13.6336 9.53305 13.6245C9.84275 13.6154 10.135 13.479 10.3408 13.2474L12.3408 10.9974L14.3408 8.74741Z" fill="#4AB393"/>
@@ -101,36 +110,36 @@ function TodoWidget() {
             `}
           />
           <Rectangle 
-            hidden={!outOfScope}
+            hidden={!props.outOfScope}
             fill={'#f2f2f2'}
             width={20}
             height={20}
           />
           <TextBlock 
-            fill={outOfScope ?  "#6E6E6E" : done ? "#767676" : "#101010"}
-            fontSize={done || outOfScope ? 13 : 14}
+            fill={ props.outOfScope ?  "#6E6E6E" :  props.done ? "#767676" : "#101010"}
+            fontSize={ props.done ||  props.outOfScope ? 13 : 14}
             lineHeight={20}
             width={180}
             onClick={() => 
               new Promise((resolve) => {
                 const widget = figma.getNodeById(widgetId)
-                figma.showUI(__uiFiles__.ui, {height: 56, title: 'Edit your todo', position: {y: widget.y - 150, x: widget.x}})
-                figma.ui.postMessage({ type: 'edit', id, title, widget })
+                figma.showUI(__uiFiles__.ui, {height: 56, title: 'Edit your todo', position: {y: (widget as WidgetNode).y - 150, x: (widget as WidgetNode).x}})
+                figma.ui.postMessage({ type: 'edit',  id: props.id,  title: props.title, widget })
               })
             }
           >
-            {title}
+            { props.title}
           </TextBlock>
         </AutoLayout>
         <AutoLayout
           onClick={() => 
             new Promise((resolve) => {
               const widget = figma.getNodeById(widgetId)
-              figma.showUI(__uiFiles__.menu, {height: 85, width: 180, title: 'Menu', position: {y: widget.y - 58, x: widget.x + widget.width + 7}})
-              figma.ui.postMessage({ type: 'menu', id, title, outOfScope, widget })
+              figma.showUI(__uiFiles__.menu, {height: 85, width: 180, title: 'Menu', position: {y: (widget as WidgetNode).y - 58, x: (widget as WidgetNode).x + (widget as WidgetNode).width + 7}})
+              figma.ui.postMessage({ type: 'menu',  id: props.id,  title: props.title,  outOfScope: props.outOfScope, widget })
             })
           }
-          fill={outOfScope ? "#f2f2f2" : "#fff"}      
+          fill={props.outOfScope ? "#f2f2f2" : "#fff"}      
         >
           <SVG
             src={`
@@ -171,43 +180,47 @@ function TodoWidget() {
           direction={'vertical'}
           spacing={8}
           width={'fill-parent'}
-        >
+          >
           {todos
             .filter(todo => !todo.done && !todo.outOfScope)
             .map(todo => 
               <Todo 
-                key={todo.key} 
-                id={todo.id} 
-                title={todo.title} 
-                done={todo.done} 
-                outOfScope={todo.outOfScope} 
+              key={todo.key} 
+              id={todo.id} 
+              title={todo.title} 
+              done={todo.done} 
+              outOfScope={todo.outOfScope} 
               />
             )
           }
-          <AutoLayout 
-            direction={'horizontal'}
-            verticalAlignItems={'center'}
-            spacing={8}
-            fill={'#fff'}
-            onClick={() => 
-              new Promise((resolve) => {
-                const id = createId()
-                createTodo(id)
-                const widget = figma.getNodeById(widgetId)
-                figma.showUI(__uiFiles__.ui, {height: 56, title: 'Add a todo', position: {y: widget.y - 150, x: widget.x}})
-                figma.ui.postMessage({ type: 'add', id, widget })
-              })
-            }
+          <AutoLayout
+            width={'fill-parent'}
           >
-            <SVG
-              src={`
+            <AutoLayout
+              direction={'horizontal'}
+              verticalAlignItems={'center'}
+              spacing={8}
+              fill={'#fff'}
+              onClick={() =>
+                new Promise((resolve) => {
+                  const id = createId()
+                  createTodo(id)
+                  const widget = figma.getNodeById(widgetId)
+                  figma.showUI(__uiFiles__.ui, {height: 56, title: 'Add a todo', position: {y: (widget as WidgetNode).y - 150, x: (widget as WidgetNode).x}})
+                  figma.ui.postMessage({ type: 'add', id, widget })
+                })
+              }
+              >
+              <SVG
+                src={`
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M10.125 5C10.7463 5 11.25 5.44772 11.25 6V14C11.25 14.5523 10.7463 15 10.125 15C9.50368 15 9 14.5523 9 14V6C9 5.44772 9.50368 5 10.125 5Z" fill="#949494"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M5 9.875C5 9.25368 5.44772 8.75 6 8.75L14 8.75C14.5523 8.75 15 9.25368 15 9.875C15 10.4963 14.5523 11 14 11L6 11C5.44772 11 5 10.4963 5 9.875Z" fill="#949494"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M10.125 5C10.7463 5 11.25 5.44772 11.25 6V14C11.25 14.5523 10.7463 15 10.125 15C9.50368 15 9 14.5523 9 14V6C9 5.44772 9.50368 5 10.125 5Z" fill="#949494"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M5 9.875C5 9.25368 5.44772 8.75 6 8.75L14 8.75C14.5523 8.75 15 9.25368 15 9.875C15 10.4963 14.5523 11 14 11L6 11C5.44772 11 5 10.4963 5 9.875Z" fill="#949494"/>
                 </svg>
-              `}
-            />
-            <TextBlock fill={'#949494'} fontSize={14} lineHeight={20} fontWeight={700} letterSpacing={'-0.75%'}>Add a todo</TextBlock>
+                `}
+                />
+              <TextBlock fill={'#949494'} fontSize={14} lineHeight={20} fontWeight={700} letterSpacing={'-0.75%'}>Add a todo</TextBlock>
+            </AutoLayout>
           </AutoLayout>
         </AutoLayout>
         <AutoLayout
